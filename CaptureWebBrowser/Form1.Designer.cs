8466d3c44d4e517ca92828d1da50b5d62fd62163
MajoranaX <m60a3214@yahoo.com.tw>
2021-02-17 04:55:30 +0800

    Ver 1.0.0.2

--- CaptureWebBrowser/Form1.Designer.cs
+++ CaptureWebBrowser/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.m_wbBrowser = new System.Windows.Forms.WebBrowser();
             this.m_bGo = new System.Windows.Forms.Button();
             this.m_bCapture = new System.Windows.Forms.Button();
@@ -142,6 +143,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
