c5565b19b36bfd26337333150b5b4167292b8f5d
MajoranaX <m60a3214@yahoo.com.tw>
2021-02-17 04:34:16 +0800

    ver 1.0.0.1

--- CaptureWebBrowser/Form1.Designer.cs
+++ CaptureWebBrowser/Form1.Designer.cs
@@ -28,13 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.m_wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.m_bGo = new System.Windows.Forms.Button();
+            this.m_bCapture = new System.Windows.Forms.Button();
+            this.m_tbURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.m_folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.m_tbMessage = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // m_wbBrowser
+            // 
+            this.m_wbBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.m_wbBrowser.Location = new System.Drawing.Point(0, 0);
+            this.m_wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.m_wbBrowser.Name = "m_wbBrowser";
+            this.m_wbBrowser.Size = new System.Drawing.Size(1476, 546);
+            this.m_wbBrowser.TabIndex = 0;
+            this.m_wbBrowser.Url = new System.Uri("http://www.google.com", System.UriKind.Absolute);
+            // 
+            // m_bGo
+            // 
+            this.m_bGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.m_bGo.Location = new System.Drawing.Point(1014, 13);
+            this.m_bGo.Name = "m_bGo";
+            this.m_bGo.Size = new System.Drawing.Size(75, 23);
+            this.m_bGo.TabIndex = 1;
+            this.m_bGo.Text = "Go";
+            this.m_bGo.UseVisualStyleBackColor = true;
+            this.m_bGo.Click += new System.EventHandler(this.m_bGo_Click);
+            // 
+            // m_bCapture
+            // 
+            this.m_bCapture.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.m_bCapture.Location = new System.Drawing.Point(1014, 42);
+            this.m_bCapture.Name = "m_bCapture";
+            this.m_bCapture.Size = new System.Drawing.Size(75, 23);
+            this.m_bCapture.TabIndex = 2;
+            this.m_bCapture.Text = "Capture";
+            this.m_bCapture.UseVisualStyleBackColor = true;
+            this.m_bCapture.Click += new System.EventHandler(this.m_bCapture_Click);
+            // 
+            // m_tbURL
+            // 
+            this.m_tbURL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.m_tbURL.Location = new System.Drawing.Point(46, 13);
+            this.m_tbURL.Name = "m_tbURL";
+            this.m_tbURL.Size = new System.Drawing.Size(952, 22);
+            this.m_tbURL.TabIndex = 3;
+            this.m_tbURL.Text = "www.google.com";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL";
+            // 
+            // m_tbMessage
+            // 
+            this.m_tbMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.m_tbMessage.Location = new System.Drawing.Point(46, 41);
+            this.m_tbMessage.Name = "m_tbMessage";
+            this.m_tbMessage.ReadOnly = true;
+            this.m_tbMessage.Size = new System.Drawing.Size(952, 22);
+            this.m_tbMessage.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.m_wbBrowser);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1476, 546);
+            this.panel1.TabIndex = 6;
+            // 
+            // splitter1
+            // 
+            this.splitter1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 546);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(1476, 10);
+            this.splitter1.TabIndex = 7;
+            this.splitter1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.m_tbURL);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.m_tbMessage);
+            this.panel2.Controls.Add(this.m_bGo);
+            this.panel2.Controls.Add(this.m_bCapture);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 556);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1476, 72);
+            this.panel2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1476, 628);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser m_wbBrowser;
+        private System.Windows.Forms.Button m_bGo;
+        private System.Windows.Forms.Button m_bCapture;
+        private System.Windows.Forms.TextBox m_tbURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog m_folderBrowserDialog;
+        private System.Windows.Forms.TextBox m_tbMessage;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- CaptureWebBrowser/Form1.cs
+++ CaptureWebBrowser/Form1.cs
@@ -3,18 +3,247 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+// add namespace
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+using System.Net;
+using Microsoft.Win32;
+
+//add reference
+using mshtml;    // Microsoft.mshtml  (set " Interop" to "false") & microsoft html object lib 
+using SHDocVw;//COM -  Microsoft Internet Controls
+
 
 namespace CaptureWebBrowser
 {
     public partial class Form1 : Form
     {
+        #region DLLImport
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr FindWindowEx(IntPtr parent /*HWND*/, IntPtr next /*HWND*/, string sClassName, IntPtr sWindowTitle);
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr GetWindow(IntPtr hWnd, int uCmd);
+
+        [DllImport("user32.dll")]
+        private static extern void GetClassName(int h, StringBuilder s, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        private static extern bool PrintWindow(IntPtr hwnd, IntPtr hdcBlt, uint nFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        public const int GW_CHILD = 5;
+        public const int GW_HWNDNEXT = 2;
+
+        #endregion
+
+        string m_sRegkeyName = @"HKEY_CURRENT_USER\Software\MAJORANA\CaptureWeb";
+        string m_sRegValueName = @"SelectedPath";
+        string m_sPath = Environment.SpecialFolder.MyPictures.ToString();
+        public bool firstScreen = true;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private ImageCodecInfo GetImageInfo(string imageCode)
+        {
+            ImageCodecInfo fICI = null;
+            foreach (ImageCodecInfo i in ImageCodecInfo.GetImageEncoders())
+            {
+                fICI = i;
+                if (i.MimeType == imageCode)
+                {
+                    fICI = i;
+                    break;
+                }
+           }
+            return fICI;
+        }
+
+        private void SetMessage(string msg)
+        {
+            m_tbMessage.Text = msg;
+        }
+
+        public void GetPageImage(string url, string path)
+        {
+            bool flag = firstScreen;
+            int URLExtraHeight = 3;
+            int URLExtraLeft = 3;
+
+            mshtml.IHTMLDocument2 myDoc = (mshtml.IHTMLDocument2)m_wbBrowser.Document.DomDocument;   
+            (myDoc as HTMLDocumentClass).documentElement.setAttribute("scroll", "yes", 0);
+            
+            //document high
+            int heightsize = (int)(myDoc as HTMLDocumentClass).documentElement.getAttribute("scrollHeight", 0);
+            int widthsize = (int)(myDoc as HTMLDocumentClass).documentElement.getAttribute("scrollWidth", 0);
+
+            ////Get Screen Height
+            int screenHeight = (int)(myDoc as HTMLDocumentClass).documentElement.getAttribute("clientHeight", 0);
+            int screenWidth = (int)(myDoc as HTMLDocumentClass).documentElement.getAttribute("clientWidth", 0);
+
+            IntPtr myIntptr = (IntPtr)m_wbBrowser.Handle;
+            IntPtr wbHandle = FindWindowEx(myIntptr, IntPtr.Zero, "Shell Embedding", null);
+            wbHandle = FindWindowEx(wbHandle, IntPtr.Zero, "Shell DocObject View", null);
+            wbHandle = FindWindowEx(wbHandle, IntPtr.Zero, "Internet Explorer_Server", null);
+            IntPtr hwnd = wbHandle;
+
+            Bitmap bm = new Bitmap(screenWidth, screenHeight, System.Drawing.Imaging.PixelFormat.Format24bppRgb);//.Format16bppRgb555);
+            Bitmap bm2 = new Bitmap(widthsize, heightsize, System.Drawing.Imaging.PixelFormat.Format24bppRgb);//.Format16bppRgb555);
+            Graphics g = null;
+            Graphics g2 = Graphics.FromImage(bm2);
+            
+            Bitmap tempbm = null;
+            Graphics tempg = null;
+
+            IntPtr hdc;
+            Image screenfrag = null;
+            Image firstScreenfrag = null;
+
+            
+            int brwTop = 0;
+            int brwLeft = 0;
+            int myPage = 0;
+
+            while ((myPage * screenHeight) < heightsize){
+                (myDoc as HTMLDocumentClass).documentElement.setAttribute("scrollTop", (screenHeight - 5) * myPage, 0);
+                ++myPage;
+            }
+            
+            --myPage;
+
+            int myPageWidth = 0;
+            
+            while ((myPageWidth * screenWidth) < widthsize){
+                (myDoc as HTMLDocumentClass).documentElement.setAttribute("scrollLeft", (screenWidth - 5) * myPageWidth, 0);
+                brwLeft = (int)(myDoc as HTMLDocumentClass).documentElement.getAttribute("scrollLeft", 0);
+                for (int i = myPage; i >= 0; --i){
+                    g = Graphics.FromImage(bm);
+                    hdc = g.GetHdc();
+                    (myDoc as HTMLDocumentClass).documentElement.setAttribute("scrollTop", (screenHeight - 5) * i, 0);
+                    brwTop = (int)(myDoc as HTMLDocumentClass).documentElement.getAttribute("scrollTop", 0);
+                    PrintWindow(hwnd, hdc, 0);
+                    g.ReleaseHdc(hdc);
+                    g.Flush();
+                    screenfrag = Image.FromHbitmap(bm.GetHbitmap());
+                    tempbm = new Bitmap(screenWidth - URLExtraLeft, screenHeight - URLExtraHeight, System.Drawing.Imaging.PixelFormat.Format24bppRgb);// Format16bppRgb555);
+                    tempg = Graphics.FromImage(tempbm);
+                    tempg.DrawImage(screenfrag, -URLExtraLeft, -URLExtraHeight);
+                    g2.DrawImage(tempbm, brwLeft + URLExtraLeft, brwTop + URLExtraLeft);
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                if (flag)
+                {
+                    firstScreenfrag = (Image)tempbm.Clone();
+                    flag = false;
+                }
+
+                ++myPageWidth;
+            }
+
+            int finalWidth = (int)widthsize;
+            int finalHeight = (int)heightsize;
+            Bitmap finalImage = new Bitmap(finalWidth, finalHeight, System.Drawing.Imaging.PixelFormat.Format24bppRgb);// Format16bppRgb555);
+            Graphics gFinal = Graphics.FromImage((Image)finalImage);
+            gFinal.DrawImage(bm2, 0, 0, finalWidth, finalHeight);
+            //Get Time Stamp
+            DateTime myTime = DateTime.Now;
+            String format = "yyyyMMdd-hhmmss";
+
+            EncoderParameters eps = new EncoderParameters(1);
+            long myQuality = Convert.ToInt64(100);
+            eps.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, myQuality);
+
+            ImageCodecInfo sICI = GetImageInfo("image/jpeg");
+            finalImage.Save(path + myTime.ToString(format) + ".jpg", sICI, eps);
+            
+            if (firstScreen) {
+                firstScreenfrag.Save(path + @"First-" + myTime.ToString(format) + ".jpg", sICI, eps);
+                firstScreenfrag.Dispose();
+            }
+
+/// Clean       
+            myDoc.close();
+            myDoc = null;
+            g.Dispose();
+            g2.Dispose();
+            gFinal.Dispose();
+            bm.Dispose();
+            bm2.Dispose();
+            finalImage.Dispose();
+            tempbm.Dispose();
+            tempg.Dispose();
+        }
+
+        private void m_bGo_Click(object sender, EventArgs e)
+        {
+            checkURL();
+            m_wbBrowser.Navigate(m_tbURL.Text);
+
+        }
+
+        private void SetFilePath()
+        {
+            string sSelectedPath = (string)Registry.GetValue(m_sRegkeyName, m_sRegValueName, m_sPath);
+            if (sSelectedPath != null)
+                m_folderBrowserDialog.SelectedPath = sSelectedPath;
+
+            if (m_folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                m_sPath = m_folderBrowserDialog.SelectedPath + @"\";
+            }
+
+            if (m_sPath.Length < 1)
+            {
+                m_sPath = Environment.SpecialFolder.MyPictures.ToString();
+                return;
+            }
+            Registry.SetValue(m_sRegkeyName, m_sRegValueName, m_sPath, RegistryValueKind.String);
+
+        }
+
+        private void checkURL()
+        {
+
+            Uri uriResult;
+            bool result = Uri.TryCreate("http://"+m_tbURL.Text, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            if (result != true)
+                m_tbURL.Text = "www.google.com";
+        }
+        private void m_bCapture_Click(object sender, EventArgs e)
+        {
+            checkURL();
+            m_wbBrowser.Navigate(m_tbURL.Text);
+
+            SetFilePath();
+            SetMessage("Processing...");
+            try
+            {
+                GetPageImage(m_tbURL.Text, m_sPath);
+                SetMessage("Finish!");
+            }
+            catch ( Exception ex)
+            {
+                SetMessage("Error:"+ex.Message);
+            }
+            
+            
         }
     }
 }
